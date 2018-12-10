c201bf29033f153daa3e81fdaf7f545493d889e9
Redemske <marc.redemske@gmail.com>
2018-12-10 17:05:17 +0100

    feat(script): add dynamic variable handling
    change script to parse replace repo and tag from given arguments

--- test-command.sh
+++ test-command.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "hello world"
-echo "got $1 $2"
+REPO=$1
+TAG=$2
+
+eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" > ./teamcity_build.xml
+curl -X POST "http://teamcity.hypoport.local/teamCity/guestAuth/app/rest/buildQueue" -H "Content-Type: application/xml" -d@teamcity_build.xml
