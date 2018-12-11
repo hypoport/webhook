009e7b076bd320dff9bab203536987aa5180e638
Tobias Gesellchen <tobias@gesellix.de>
2018-12-11 12:05:31 +0100

    don't rely on the file system

--- trigger-teamcity-build.sh
+++ trigger-teamcity-build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/ash
 
 REPO=$1
 TAG=$2
 BUILD_TYPE_ID=$3
 
-# todo in-memory behalten, falls viele Requests reinkommen
-eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" > ./teamcity_build.xml
-curl -X POST "http://teamcity.hypoport.local/teamCity/guestAuth/app/rest/buildQueue" -H "Content-Type: application/xml" -d@teamcity_build.xml
+eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" | curl -X POST "http://teamcity.hypoport.local/teamCity/guestAuth/app/rest/buildQueue" \
+     -H "Content-Type: application/xml" \
+     -d @-
