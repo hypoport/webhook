8f77a8d761cd409bd91e604312bc33badd8e4527
Tobias Gesellchen <tobias@gesellix.de>
2018-12-11 14:36:09 +0100

    hackedy hack for slack

--- trigger-teamcity-build.sh
+++ trigger-teamcity-build.sh
@@ -4,6 +4,15 @@ REPO=$1
 TAG=$2
 BUILD_TYPE_ID=$3
 
+if [[ "$#" -eq 1 ]]
+then
+  echo "hmmmm"
+  eval "ARGUMENTS=($1)"
+  REPO=${ARGUMENTS[0]}
+  TAG=${ARGUMENTS[1]}
+  BUILD_TYPE_ID=${ARGUMENTS[2]}
+fi
+
 eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" | curl -X POST "http://teamcity.hypoport.local/teamCity/guestAuth/app/rest/buildQueue" \
      -H "Content-Type: application/xml" \
      -d @-
