4908bf10ec235ead26b477938a09979e9cdb22a3
Tobias Gesellchen <tobias@gesellix.de>
2018-12-11 22:14:58 +0100

    be more POSIX compliant

--- trigger-teamcity-build.sh
+++ trigger-teamcity-build.sh
@@ -1,17 +1,17 @@
-#!/bin/ash
+#!/bin/sh
 
 REPO=$1
 TAG=$2
 BUILD_TYPE_ID=$3
+TEAMCITY_BASE_URL="http://teamcity.hypoport.local/teamCity"
 
 if [[ "$#" -eq 1 ]]
 then
-  eval "ARGUMENTS=($1)"
-  REPO=${ARGUMENTS[0]}
-  TAG=${ARGUMENTS[1]}
-  BUILD_TYPE_ID=${ARGUMENTS[2]}
+  REPO=$(echo $1 | cut -d\  -f1)
+  TAG=$(echo $1 | cut -d\  -f2)
+  BUILD_TYPE_ID=$(echo $1 | cut -d\  -f3)
 fi
 
-eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" | curl -X POST "http://teamcity.hypoport.local/teamCity/guestAuth/app/rest/buildQueue" \
+eval "echo \"$(cat ./teamcity_build.xml.tmpl)\"" | curl -X POST "${TEAMCITY_BASE_URL}/guestAuth/app/rest/buildQueue" \
      -H "Content-Type: application/xml" \
      -d @-
